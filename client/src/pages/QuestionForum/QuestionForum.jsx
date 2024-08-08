3d737e3a46b8fcaaade3c5cf119cb7c427e100ba
Hayato <khayato0512.canada@gmail.com>
2024-08-07 17:22:26 -0700

    save: Css progress in QuestionForum.

--- client/src/pages/QuestionForum/QuestionForum.jsx
+++ client/src/pages/QuestionForum/QuestionForum.jsx
@@ -1,32 +1,72 @@
-import React from "react";
+import { React, useRef, useState } from "react";
 // import FormControl from "@mui/material";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
+import "./questionForum.css";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 export default function QuestionForum() {
-  const [age, setAge] = React.useState("");
+  const [age, setAge] = useState("");
+  const inputRefName = useRef();
 
   const handleChange = (event) => {
     setAge(event.target.value);
   };
+
+  const questionCreateClicked = () => {
+    console.log(
+      "questionCreateClicked, now, the ref is this",
+      inputRefName.current.value
+    );
+  };
+
   return (
-    <div>
-      <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Age</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={age}
-          label="Age"
-          onChange={handleChange}
-        >
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-      </FormControl>
-      aaaaaaaaaaQuestionForum Under construnction
+    <div className="questionForum">
+      <div className="questionForumContainer">
+        <div className="questionForumTitleContainer">
+          <div className="questionForumTitle">Question Forum</div>
+        </div>
+        <div className="questionCreateButtonDiv">
+          <div className="questionCreateButton">
+            <button
+              onClick={questionCreateClicked}
+              className="questionCreateButton"
+            >
+              + Post Question
+            </button>
+          </div>
+        </div>
+        <div className="questionSearchInputDiv">
+          <input
+            type="text"
+            ref={inputRefName}
+            placeholder="Search Question"
+            className="questionSearchInputField"
+          />
+          <button
+            onClick={questionCreateClicked}
+            className="questionSearchButton"
+          >
+            create
+          </button>
+        </div>
+        <div className="questionForumFilterContainer">
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Filter</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={age}
+              label="Age"
+              onChange={handleChange}
+            >
+              <MenuItem value={10}>oldest</MenuItem>
+              <MenuItem value={20}>most popular</MenuItem>
+              <MenuItem value={30}>newest</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+      </div>
     </div>
   );
 }
