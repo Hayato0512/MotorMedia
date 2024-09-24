7b5710b7045ef6634c51e10ef5a1c6a49f5b83a3
Hayato <khayato0512.canada@gmail.com>
2024-09-24 08:23:59 -0700

    starting to integrate AWS S3. https://docs.aws.amazon.com/AmazonS3/latest/userguide/uploading-an-object-bucket.html next time start with this link to proceed with setting up the AWS.

--- client/src/hooks/useFetchQuestions.js
+++ client/src/hooks/useFetchQuestions.js
@@ -17,6 +17,7 @@ const useFetchQuestions = (currentUser, tags) => {
         const res = await axiosInstance.post("/questions/tags", { tags });
         setQuestionList(res.data);
       }
+      //
     } catch (error) {
       console.error("Error fetching questions:", error);
     }
